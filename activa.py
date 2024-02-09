b17efcf94b3a2dd19893d46d6a2b3699f8be04a6
HaukeFalcon <156836594+HaukeFalcon@users.noreply.github.com>
2024-02-09 14:57:33 -0600

    Add files via upload

--- activa.py
+++ activa.py
@@ -1,16 +1,10 @@
-
-
 from datetime import datetime, timedelta
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
 import plotly.express as px
-import pip 
-
-
-pip.main(['install', 'plotly_express'])
-pip.main(["install","openpyxl"])
+import pip
 
 def reemplazos(v):
   v=str(v)
@@ -20,7 +14,7 @@ def reemplazos(v):
 pip.main(['install', 'plotly_express'])
 pip.main(["install","openpyxl"])
 
-st.title("FUNNEL Y TRACKING DE INVER+")
+st.title("ACTIVA 1.0")
 st.text('La presente página permite visualizar el comportamiento generalizado de los socios en la aplicacion.')
 #df=pd.read_excel('PROCESOS_PIVOTE.xlsx')
 
@@ -47,39 +41,39 @@ chart_data = chart_data.set_index('FECHA')
 st.bar_chart(chart_data)
 #st.image('BARRAS.png',caption='EVOLUCION DE LA ACTIVIDAD')
 
-st.text('Estos valores corresponden a los ingresos identificados en las siguientes ubicaciones.')
-df=pd.read_csv('COORDENADAS (1).csv',encoding='latin-1')
-df=df.rename(columns={'LATITUD':'lat','LONGITUD':'lon'})
-chart_data = df
+#st.text('Estos valores corresponden a los ingresos identificados en las siguientes ubicaciones.')
+#df=pd.read_csv('COORDENADAS (1).csv',encoding='latin-1')
+#df=df.rename(columns={'LATITUD':'lat','LONGITUD':'lon'})
+#chart_data = df
 
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=19.69816,
-        longitude=-101.15816,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=chart_data,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[0]',
-            get_radius=200,
-        ),
-    ],
-))
+#st.pydeck_chart(pdk.Deck(
+ #   map_style=None,
+  #  initial_view_state=pdk.ViewState(
+   #     latitude=19.69816,
+    #    longitude=-101.15816,
+     #   zoom=11,
+      #  pitch=50,
+    #),
+#    layers=[
+ #       pdk.Layer(
+  #         'HexagonLayer',
+   #        data=chart_data,
+    #       get_position='[lon, lat]',
+     #      radius=200,
+      #     elevation_scale=4,
+#           elevation_range=[0, 1000],
+ #          pickable=True,
+  #         extruded=True,
+   #     ),
+    #    pdk.Layer(
+     #       'ScatterplotLayer',
+      #      data=chart_data,
+       #     get_position='[lon, lat]',
+        #    get_color='[0]',
+         #   get_radius=200,
+#        ),
+ #   ],
+#))
 
 st.markdown(f' SISTEMA OPERATIVO')
 df=pd.read_csv('SO (1).csv',encoding='latin-1')
@@ -88,10 +82,10 @@ st.text('Las visitas obtenidas corresponden al uso de los siguientes sistemas op
 fig=px.pie(df, values='FRECUENCIA', names='SISTEMA')#,width=500,height=400)
 st.plotly_chart(fig)
 
-st.markdown(f' NAVEGADOR')
-df=pd.read_csv('NAVEGADOR (1).csv',encoding='latin-1')
-df=df.drop('Unnamed: 0',axis=1)
-st.text('Como enfoque adicional se puede identificar el navegador implementado como se muestra a continuación.')
-fig=px.pie(df,values='FRECUENCIA',names='NAVEGADOR',width=500,height=400)
+#st.markdown(f' NAVEGADOR')
+#df=pd.read_csv('NAVEGADOR (1).csv',encoding='latin-1')
+#df=df.drop('Unnamed: 0',axis=1)
+#st.text('Como enfoque adicional se puede identificar el navegador implementado como se muestra a continuación.')
+#fig=px.pie(df,values='FRECUENCIA',names='NAVEGADOR',width=500,height=400)
 #,width=500,height=400)
-st.plotly_chart(fig)
+#st.plotly_chart(fig)
